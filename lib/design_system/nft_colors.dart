3446f120e8f27e6d3900544d8d59be94974166cc
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-20 22:35:58 -0300

    create design system: colors

---
+++ lib/design_system/nft_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/services.dart';
+
+class NftColors {
+  NftColors._();
+
+  static const Color primary = Color(0xFF1D9BF0);
+  static const Color secondary = Color(0xFF253341);
+  static const Color sucess = Color(0xFF00CB6A);
+  static const Color danger = Color(0xFFF26666);
+  static const Color dark = Color(0xFF15202B);
+  static const Color grayLight = Color(0xFFAAB8C2);
+  static const Color white = Color(0xFFF5F8FA);
+}
