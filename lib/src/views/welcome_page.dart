89e733949271f5d932963c10d2c7d5c7f85d0bc9
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-20 22:40:10 -0300

    add welcome page

---
+++ lib/src/views/welcome_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/welcome.png',
+              height: 182,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Hey! Welcome',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Create and Sell your NFT in 1 seconds',
+              style: Theme.of(context).textTheme.bodyText1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
