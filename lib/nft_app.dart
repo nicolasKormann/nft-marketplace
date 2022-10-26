4a2d38715528cb04612d8494e1f07e7ccc377d50
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-26 15:22:35 -0300

    feat: add flutter_native_splash

--- lib/nft_app.dart
+++ lib/nft_app.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:nft/design_system/nft_colors.dart';
 import 'package:nft/design_system/nft_theme.dart';
 import 'package:nft/src/views/welcome_page.dart';
 import 'package:nft/src/views/login_screen.dart';
-import 'package:nft/src/views/splash_screen.dart';
 import 'package:nft/src/views/start_screen.dart';
 
-class NftApp extends StatelessWidget {
+class NftApp extends StatefulWidget {
   const NftApp({super.key});
+
+  @override
+  State<NftApp> createState() => _NftAppState();
+}
+
+class _NftAppState extends State<NftApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+   void initialization() async {
+    FlutterNativeSplash.remove();
+   }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +35,7 @@ class NftApp extends StatelessWidget {
       theme: NftAppTheme.nftTheme,
       initialRoute: '/',
       routes: {
-        '/': (context) => const SplashScreen(),
-        '/start': (context) => const StartScreen(),
+        '/': (context) => const StartScreen(),
         '/login': (context) => const LoginScreen(),
         '/welcome': (context) => const WelcomePage(),
       },
